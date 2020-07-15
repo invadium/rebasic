ad7e39d77513b2b9a72b6ae7b710d10628f2b207
Igor Khotin <khotin@gmx.com>
2020-07-15 12:27:41 +0300

    [+] help for math functions

--- js/lib/math.js
+++ js/lib/math.js
@@ -14,6 +14,44 @@ const fn = {
     sqr: Math.sqrt,
 }
 
+fn.abs.usage = '(x)'
+fn.abs.man = 'absolute value of the number'
+
+fn.rnd.usage = '()'
+fn.rnd.man = 'random number in the range 0-1'
+
+fn.sin.usage = '(x)'
+fn.sin.man = 'sine of [x]'
+
+fn.cos.usage = '(x)'
+fn.cos.man = 'cosine of [x]'
+
+fn.tan.usage = '(x)'
+fn.tan.man = 'tangent of [x]'
+
+fn.atn.usage = '(x)'
+fn.atn.man = 'angle in radians for tangent of [x]'
+
+fn.atn2.usage = '(y, x)'
+fn.atn2.man = 'angle to the ray passing [y, x]'
+
+fn['int'].usage = '(x)'
+fn['int'].man = 'truncate the value of [x]\n'
+    + '    the result is an integer value\n'
+    + '    always less than [x]'
+
+fn.exp.usage = '(x)'
+fn.exp.man = 'value of e raised to the power of x'
+
+fn.log.usage = '(x)'
+fn.log.man = 'natural log of [x]'
+
+fn.sgn.usage = '(x)'
+fn.sgn.man = 'sign of [x] => -1/0/+1'
+
+fn.sqr.usage = '(x)'
+fn.sqr.man = 'square root of [x]'
+
 const scope = {
     pi: Math.PI,
     tau: Math.PI * 2,
