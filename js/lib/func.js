442d5e447d595891d20dd399b9bbcd6db0762a29
Igor Khotin <khotin@gmx.com>
2025-08-14 01:55:32 +0300

    [+] time() function

--- js/lib/func.js
+++ js/lib/func.js
@@ -5,9 +5,15 @@ const func = {
         return (this.ram[n] || 0)
     },
 
+    time: function() {
+        return Date.now()
+    },
 }
 
 func.peek.usage = 'address'
 func.peek.man = 'get content of memory at [address]'
+
+func.time.usage = ''
+func.time.man = 'return current time in milliseconds'
 
 module.exports = func
